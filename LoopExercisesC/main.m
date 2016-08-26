f2a14d2a8fd5dafabedcd3882672205e9f23715e
Ewa Elamiri <ewa.czekalska@gmail.com>
2016-08-26 07:27:35 +0100

    first commit

---
+++ LoopExercisesC/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  LoopExercisesC
+//
+//  Created by User on 26/08/2016.
+//  Copyright © 2016 User. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
